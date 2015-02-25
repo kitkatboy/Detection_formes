53cf0c9c5f618c462549d11c86bfe4650e7915d9
greg-nicolle <greg.nicolle@gmail.com>
2015-02-25 23:34:57 +0100

    fin de la question un peu moins beugué

--- src/Extractor.cpp
+++ src/Extractor.cpp
@@ -21,35 +21,42 @@ void Extractor::run() {
     positions_app = get_positions(app);
 
     positions_test = get_positions(test);
+
+    std::cout<<positions_test[5 * 40 + 11 * 2+1].second<<std::endl;
 //    namedWindow( "test", WINDOW_NORMAL);
 //    imshow("test",test);
 //    waitKey(0);
-    std::cout<<"ok1"<<std::endl;
-    for(int i = 0; i < 10; i++) {
+//    std::cout<<"ok1"<<std::endl;
+    for(int i = 0; i < 1; i++) {
 
         tmp.resize(0);
 
         for(int j = 0; j < 20; j++) {
 //            std::cout<<"ok2"<<std::endl;
             tmp.push_back(profil(positions_app[i*40 + j*2], positions_app[i*40 + j*2 + 1],app));
+//            std::cout<< tmp.at(j).size()<<std::endl;
 
-            show_element(i, j,test,positions_test);
+//            show_element(i, j,app,positions_app);
 
             // TODO Moyenne de classe
 
         }
-        std::cout<<"ok3-------------"<<std::endl;
-        for(int k =0;k<moyenne(tmp).size();k++){
+//        std::cout<<moyenne(tmp).size()<<std::endl;
+//        for(int k =0;k<moyenne(tmp).size();k++){
 //            std::cout<< moyenne(tmp).at(k)<<std::endl;
-        }
+//        }
         moy_class.push_back(moyenne(tmp));
     }
+//    std::cout<<"tralala"<<moy_class.size()<<std::endl;
 
     std::cout<<"ok"<<std::endl;
-    for(int i = 0; i < 10; i++) {
+    for(int i = 5; i < 6; i++) {
+
         for (int j = 0; j < 20; j++) {
-            show_element(i, j,test,positions_test);
+//            show_element(i, j,test,positions_test);
+//            std::cout<<positions_test[5 * 40 + 0 * 2+1].second<<std::endl;
             std::cout<<"i : "<<i<<" j : "<<j<<" classe : "<<proba(profil(positions_test[i * 40 + j * 2], positions_test[i * 40 + j * 2 + 1], test))<<std::endl;
+//            std::cout<<"i : "<<i<<" j : "<<j<<" classe : "<<proba(profil(positions_app[i * 40 + j * 2], positions_app[i * 40 + j * 2 + 1], app))<<std::endl;
         }
     }
 
@@ -60,17 +67,22 @@ void Extractor::run() {
 
 std::vector<float> Extractor::moyenne(std::vector<std::vector<float>> entree) {
     float cpt;
-    std::vector<float> moyenne;
-    for(int i = 0; i< entree.size();i++) {
+    int j;
+    std::vector<float> moy;
+    for(int i = 0; i< entree.at(0).size();i++) {
         cpt = 0;
-        for(int j = 0; j < entree.at(i).size(); j++) {
-            cpt+=entree.at(i).at(j);
-            std::cout<<entree.at(i).at(j)<<std::endl;
+        for(j = 0; j < entree.size(); j++) {
+            cpt+=entree.at(j).at(i);
+//            std::cout<<entree.at(i).at(j)<<std::endl;
         }
+//        std::cout<<entree.at(i).size()<<std::endl;
 
-        moyenne.push_back(cpt/entree.at(i).size());
+        moy.push_back(cpt/entree.at(0).size());
     }
-    return moyenne;
+    if(moy.size() != 10){
+        std::cout<<"c'est la merde"<<std::endl;
+    }
+    return moy;
 }
 
 
@@ -157,8 +169,8 @@ std::vector< std::pair<int, int> > Extractor::get_positions(Mat source) {
                         }
 
                     }
-                    if(haut_gauche.second > bas_droit.second) std::cout<<":("<<std::endl;
-                    if(haut_gauche.first > bas_droit.first) std::cout<<"   :("<<std::endl;
+                    if(haut_gauche.second >= bas_droit.second) std::cout<<":("<<std::endl;
+                    if(haut_gauche.first >= bas_droit.first) std::cout<<"   :("<<std::endl;
                 }
             }
             tot_lines++;
@@ -230,42 +242,33 @@ void Extractor::show_histo(Mat&img, int choice) {
 std::vector<float> Extractor::profil(std::pair<int,int> haut_gauche, std::pair<int,int> bas_droit,Mat source) {
     int j;
     std::vector<float> result;
-
-//    std::cout << "Go..." << std::endl;
-//    std::cout << "deb : " << haut_gauche.second << std::endl;
-//    std::cout << "fin : " << bas_droit.second << std::endl;
-//    std::cout << "nb loop : " << ((bas_droit.second - haut_gauche.second) / 5.0) + 0.5  << std::endl;
-
-    for(int i = haut_gauche.second; i < bas_droit.second; i += ((bas_droit.second - haut_gauche.second) / 5.0) + 0.5) {
-
-//        std::cout << "loop : " << i << std::endl;
-
-//        std::cout << "HG : " << haut_gauche.first << std::endl;
-//        std::cout << "BD : " << bas_droit.first << std::endl;
-
+    std::cout<<"       "<<(bas_droit.first - haut_gauche.first)<<std::endl;
+    for(int i = 0; i < 5; i += 1) {
         for(j = haut_gauche.first ; j < bas_droit.first; j++) {
-
-//            std::cout << "Attention en dessous c de la merde" << std::endl;
-            if((int) source.at<unsigned char>(i, j) == 0) {
-//                std::cout << "Ce qu'on ne verra jamais" << std::endl;
+            if((int) source.at<unsigned char>(haut_gauche.second+i*(((bas_droit.second - haut_gauche.second) / 5.0) + 0.5), j) == 0) {
                 break;
             }
         }
         result.push_back((bas_droit.first - haut_gauche.first) / (float)j);
-
-//        std::cout << "Param 1 " <<(bas_droit.first - haut_gauche.first) / (float)j<< std::endl;
     }
 
-    for(int i = haut_gauche.second; i < bas_droit.second; i += ((bas_droit.second - haut_gauche.second) / 5.0) + 0.5) {
+    for(int i = 0; i < 5; i += 1) {
         for(j = bas_droit.first ; j > haut_gauche.first ; j--) {
-            if((int)source.at<unsigned char>(i, j) == 0) {
+            if((int)source.at<unsigned char>(haut_gauche.second + i*(((bas_droit.second - haut_gauche.second) / 5.0) + 0.5), j) == 0) {
                 break;
             }
         }
         result.push_back((bas_droit.first - haut_gauche.first) / (float)j);
-
     }
-//    std::cout<<"       "<<result.at(1)<<std::endl;
+//    if(result.size() != 10){
+//        std::cout << "Go..." << std::endl;
+//        std::cout << "deb : " << haut_gauche.second << std::endl;
+//        std::cout << "fin : " << bas_droit.second << std::endl;
+//        std::cout << "nb loop : " << (int)((bas_droit.second - haut_gauche.second) / 5.0) + 0.5  << std::endl;
+//        std::cout << "nb loop : " << (int)(((bas_droit.second - haut_gauche.second) / 5.0) + 0.5)  << std::endl;
+//        std::cout << "j : "<<j<<std::endl;
+//    }
+
     return result;
 }
 
@@ -277,9 +280,10 @@ int Extractor::proba(std::vector<float> a_classer){
 
     for(int i =0 ; i < moy_class.size(); i++){
         sum = 0;
+        std::cout<<a_classer.at(i)<<std::endl;
         for(int j = 0; j < moy_class.size(); j++){
 
-//            std::cout<<"         1"<<std::endl;
+//            std::cout<<moy_class.at(j).size()<<std::endl;
             sum += exp(-distance_euclidienne(a_classer,moy_class.at(j)));
 
 //            std::cout<<"         4"<<std::endl;
@@ -289,23 +293,29 @@ int Extractor::proba(std::vector<float> a_classer){
         tmp = exp(-distance_euclidienne(a_classer,moy_class.at(i)))/sum;
 
 //        std::cout<<"         44"<<std::endl;
-//        std::cout<<tmp<<std::endl;
+//        std::cout<< distance_euclidienne(a_classer,moy_class.at(i))<<std::endl;
         if(tmp > result) {
             result = tmp;
             rclass = i;
         }
     }
-    std::cout<<result<<std::endl;
+//    std::cout<<result<<std::endl;
     return rclass;
 
 }
 
 float Extractor::distance_euclidienne(std::vector<float> X, std::vector<float> Y){
     float sum = 0;
-//    std::cout<<"         2"<<std::endl;
+//    std::cout<<"X : "<<X.at(0)<<" Y : "<<Y.at(0)<<std::endl;
+
+    if(X.size() != Y.size()) {
+        std::cout<<"calcul distance euclidienne impossible"<<std::endl;
+//        exit -1;
+    }
     for(int i = 0; i < X.size(); i++){
         sum += (X.at(i) - Y.at(i))*(X.at(i) - Y.at(i));
     }
-//    std::cout<<"         3"<<std::endl;
+//    std::cout <<sum<<std::endl;
+    if(sum<0) std::cout <<"c'est la merde dans la distance euclidienne"<<std::endl;
     return sqrt(sum);
 }
