fd7939e39ca02b93b922de7759d79a0e4e92b06d
greg-nicolle <greg.nicolle@gmail.com>
2015-02-26 16:50:46 +0100

    version final classe distance

--- include/Distance.h
+++ include/Distance.h
@@ -4,22 +4,22 @@
 
 #include <cv.h>
 #include <vector>
-//#include <math.h>
+#include <math.h>
 
 
 class Distance
 {
 protected:
-    std::vector<std::vector<float>> moy_class;
+    std::vector<std::vector<double>> moy_class;
 
 public:
     Distance();
     ~Distance();
-    void run();
-    std::vector<float> profil(std::pair<int,int> haut_gauche, std::pair<int,int> bas_droit, cv::Mat& source);
-    std::vector<float> moyenne(std::vector<std::vector<float>> entree);
-    float distance_euclidienne(std::vector<float> X, std::vector<float> Y);
-    int proba(std::vector<float> a_classer);
+    void run(std::vector< std::pair<int, int> >* positions_app,std::vector< std::pair<int, int> >* positions_test,cv::Mat app,cv::Mat test);
+    std::vector<double> profil(std::pair<int,int> haut_gauche, std::pair<int,int> bas_droit, cv::Mat& source);
+    std::vector<double> moyenne(std::vector<std::vector<double>> *entree);
+    double distance_euclidienne(std::vector<double> X, std::vector<double> Y);
+    int proba(std::vector<double> a_classer);
 };
 
 
--- src/Distance.cpp
+++ src/Distance.cpp
@@ -1,6 +1,5 @@
 #include "Distance.h"
 
-
 Distance::Distance() {
 }
 
@@ -9,154 +8,110 @@ Distance::~Distance() {
 }
 
 
-void Distance::run() {
+void Distance::run(std::vector< std::pair<int, int> >* positions_app,std::vector< std::pair<int, int> >* positions_test,cv::Mat app,cv::Mat test) {
 
-    std::vector< std::vector<float> > tmp;
+    std::vector< std::vector<double> > *tmp;
 
-    for(int i = 0; i < 1; i++) {
+    for(int i = 0; i < 10; i++) {
 
-        tmp.resize(0);
+        tmp = new std::vector< std::vector<double> >;
 
         for(int j = 0; j < 20; j++) {
-//            std::cout<<"ok2"<<std::endl;
-//            tmp.push_back(profil(positions_app[i*40 + j*2], positions_app[i*40 + j*2 + 1],app));
-//            std::cout<< tmp.at(j).size()<<std::endl;
-
-//            show_element(i, j,app,positions_app);
-
-            // TODO Moyenne de classe
-
+            tmp->push_back(profil(positions_app->at((i * positions_app->size() / 10) + (j * 2)), positions_app->at((i * positions_app->size() / 10) + (j * 2)+1),app));
         }
-//        std::cout<<moyenne(tmp).size()<<std::endl;
-//        for(int k =0;k<moyenne(tmp).size();k++){
-//            std::cout<< moyenne(tmp).at(k)<<std::endl;
-//        }
         moy_class.push_back(moyenne(tmp));
     }
-//    std::cout<<"tralala"<<moy_class.size()<<std::endl;
 
-    std::cout<<"ok"<<std::endl;
-    for(int i = 5; i < 6; i++) {
+    int cpt;
+    int cptg = 0;
 
-        for (int j = 0; j < 20; j++) {
-//            show_element(i, j,test,positions_test);
-//            std::cout<<positions_test[5 * 40 + 0 * 2+1].second<<std::endl;
-//            std::cout<<"i : "<<i<<" j : "<<j<<" classe : "<<proba(profil(positions_test[i * 40 + j * 2], positions_test[i * 40 + j * 2 + 1], test))<<std::endl;
-//            std::cout<<"i : "<<i<<" j : "<<j<<" classe : "<<proba(profil(positions_app[i * 40 + j * 2], positions_app[i * 40 + j * 2 + 1], app))<<std::endl;
+    for(int i = 0; i < 10; i++) {
+
+        cpt=0;
+
+        for (int j = 0; j < 10; j++) {
+            if(i!=proba(profil(positions_test->at((i * positions_test->size() / 10) + (j * 2)), positions_test->at((i * positions_test->size() / 10) + (j * 2)+1), test))) {
+                cpt++;
+            }
         }
+        cptg += cpt;
+        std::cout<<"il y a : "<<cpt<<" erreurs pour les "<<i<<std::endl;
     }
+    std::cout<<"il y a : "<<cptg<<"% erreurs au total"<<std::endl;
 }
 
 
-std::vector<float> Distance::moyenne(std::vector< std::vector<float> > entree) {
-    float cpt;
+std::vector<double> Distance::moyenne(std::vector< std::vector<double> > *entree) {
+    double cpt;
     int j;
-    std::vector<float> moy;
+    std::vector<double> moy;
 
-    for(int i = 0; i< entree.at(0).size();i++) {
+    for(int i = 0; i< entree->at(0).size();i++) {
         cpt = 0;
 
-        for(j = 0; j < entree.size(); j++) {
-            cpt+=entree.at(j).at(i);
-//            std::cout<<entree.at(i).at(j)<<std::endl;
+        for(j = 0; j < entree->size(); j++) {
+            cpt+=entree->at(j).at(i);
         }
-//        std::cout<<entree.at(i).size()<<std::endl;
-
-        moy.push_back(cpt/entree.at(0).size());
-    }
-
-    if(moy.size() != 10){
-        std::cout<<"c'est la merde"<<std::endl;
+        moy.push_back(cpt/entree->at(0).size());
     }
 
     return moy;
 }
 
 
-std::vector<float> Distance::profil(std::pair<int,int> haut_gauche, std::pair<int,int> bas_droit, cv::Mat& source) {
+std::vector<double> Distance::profil(std::pair<int,int> haut_gauche, std::pair<int,int> bas_droit, cv::Mat& source) {
     int j;
-    std::vector<float> result;
+    std::vector<double> result;
+    int d = 9;
 
-    std::cout<<"    "<<(bas_droit.first - haut_gauche.first)<<std::endl;
-
-    for(int i = 0; i < 5; i += 1) {
-        for(j = haut_gauche.first ; j < bas_droit.first; j++) {
-            if((int) source.at<unsigned char>(haut_gauche.second+i*(((bas_droit.second - haut_gauche.second) / 5.0) + 0.5), j) == 0) {
+    for(int i = 1; i < d+1; i += 1) {
+        for(j = haut_gauche.first; j < bas_droit.first; j++) {
+            if((int) source.at<unsigned char>(haut_gauche.second+i*(((bas_droit.second - haut_gauche.second) / (double)(d+2)) + 0.5), j) == 0) {
                 break;
             }
         }
-        result.push_back((bas_droit.first - haut_gauche.first) / (float)j);
+        result.push_back((double)(j - haut_gauche.first)/(bas_droit.first - haut_gauche.first));
     }
-
-    for(int i = 0; i < 5; i += 1) {
+    for(int i = 1; i < d+1; i += 1) {
         for(j = bas_droit.first ; j > haut_gauche.first ; j--) {
-            if((int)source.at<unsigned char>(haut_gauche.second + i*(((bas_droit.second - haut_gauche.second) / 5.0) + 0.5), j) == 0) {
+            if((int)source.at<unsigned char>(haut_gauche.second + i*(((bas_droit.second - haut_gauche.second) / (double)(d+2)) + 0.5), j) == 0) {
                 break;
             }
         }
-        result.push_back((bas_droit.first - haut_gauche.first) / (float)j);
+        result.push_back( (double)(bas_droit.first-j)/(bas_droit.first - haut_gauche.first));
     }
-//    if(result.size() != 10){
-//        std::cout << "Go..." << std::endl;
-//        std::cout << "deb : " << haut_gauche.second << std::endl;
-//        std::cout << "fin : " << bas_droit.second << std::endl;
-//        std::cout << "nb loop : " << (int)((bas_droit.second - haut_gauche.second) / 5.0) + 0.5  << std::endl;
-//        std::cout << "nb loop : " << (int)(((bas_droit.second - haut_gauche.second) / 5.0) + 0.5)  << std::endl;
-//        std::cout << "j : "<<j<<std::endl;
-//    }
-
     return result;
 }
 
 
-int Distance::proba(std::vector<float> a_classer){
-    float sum;
-    float result = -1;
-    float tmp;
+int Distance::proba(std::vector<double> a_classer){
+    double sum;
+    double result = -1;
+    double tmp;
     int rclass;
 
     for(int i =0 ; i < moy_class.size(); i++){
         sum = 0;
-        std::cout<<a_classer.at(i)<<std::endl;
-
         for(int j = 0; j < moy_class.size(); j++){
-
-//            std::cout<<moy_class.at(j).size()<<std::endl;
             sum += exp(-distance_euclidienne(a_classer,moy_class.at(j)));
-
-//            std::cout<<"         4"<<std::endl;
         }
-
-//        std::cout<<"         11"<<std::endl;
         tmp = exp(-distance_euclidienne(a_classer,moy_class.at(i)))/sum;
-
-//        std::cout<<"         44"<<std::endl;
-//        std::cout<< distance_euclidienne(a_classer,moy_class.at(i))<<std::endl;
         if(tmp > result) {
             result = tmp;
             rclass = i;
         }
     }
-//    std::cout<<result<<std::endl;
     return rclass;
 }
 
 
-float Distance::distance_euclidienne(std::vector<float> X, std::vector<float> Y){
-    float sum = 0;
-//    std::cout<<"X : "<<X.at(0)<<" Y : "<<Y.at(0)<<std::endl;
-
+double Distance::distance_euclidienne(std::vector<double> X, std::vector<double> Y){
+    double sum = 0;
     if(X.size() != Y.size()) {
         std::cout<<"calcul distance euclidienne impossible"<<std::endl;
-//        exit -1;
     }
-
     for(int i = 0; i < X.size(); i++){
         sum += (X.at(i) - Y.at(i))*(X.at(i) - Y.at(i));
     }
-
-//    std::cout <<sum<<std::endl;
-    if(sum<0) std::cout <<"c'est la merde dans la distance euclidienne"<<std::endl;
-
     return sqrt(sum);
 }
